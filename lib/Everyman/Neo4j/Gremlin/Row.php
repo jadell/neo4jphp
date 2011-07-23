6fe22a920503a0b33a6ffd606483ea126835e1f7
Josh Adell <josh.adell@gmail.com>
2011-07-23 11:49:47 -0400

    Gremlin result row.
    Don't entity map values until they are requested; prevents potentially expensive calls until a value is actually needed.

---
+++ lib/Everyman/Neo4j/Gremlin/Row.php
@@ -0,0 +1,105 @@
+<?php
+namespace Everyman\Neo4j\Gremlin;
+
+use Everyman\Neo4j\EntityMapper,
+    Everyman\Neo4j\Client;
+
+/**
+ * Represents a single result row from a Gremlin query.
+ * You can loop over this to get each value, or you can
+ * use array access ($myRow['columnName'] or $myRow[0])
+ * to get specific fields.
+ */
+class Row implements \Iterator, \Countable, \ArrayAccess
+{
+	protected $client = null;
+	protected $raw = null;
+	protected $data = null;
+	protected $columns = null;
+	protected $position = 0;
+
+	/**
+	 * Set the raw result data of this row
+	 *
+	 * @param Client $client
+	 * @param array $columns
+	 * @param array $rowData
+	 */
+	public function __construct(Client $client, $columns, $rowData)
+	{
+		$this->client = $client;
+		$this->raw = $rowData;
+		$this->data = array();
+		$this->columns = $columns;
+	}
+
+	// ArrayAccess API
+
+	public function offsetExists($offset)
+	{
+		if (!is_integer($offset)) {
+			return in_array($offset, $this->columns);
+		}
+
+		return isset($this->raw[$offset]);
+	}
+
+	public function offsetGet($offset)
+	{
+		if (!is_integer($offset)) {
+			$offset = array_search($offset, $this->columns);
+		}
+
+		if (!isset($this->data[$offset])) {
+			$this->data[$offset] = $this->client->getEntityMapper()->getEntityFor($this->raw[$offset]);
+		}
+
+		return $this->data[$offset];
+	}
+
+	public function offsetSet($offset, $value)
+	{
+		throw new \BadMethodCallException("You cannot modify a result row.");
+	}
+
+	public function offsetUnset($offset)
+	{
+		throw new \BadMethodCallException("You cannot modify a result row.");
+	}
+
+
+	// Countable API
+
+	public function count()
+	{
+		return count($this->raw);
+	}
+
+
+	// Iterator API
+
+	public function rewind()
+	{
+		$this->position = 0;
+	}
+
+	public function current()
+	{
+		return $this[$this->position];
+	}
+
+	public function key()
+	{
+		return $this->columns[$this->position];
+	}
+
+	public function next()
+	{
+		++$this->position;
+	}
+
+	public function valid()
+	{
+		return isset($this->raw[$this->position]);
+	}
+}
---
+++ tests/unit/lib/Everyman/Neo4j/Gremlin/RowTest.php
@@ -0,0 +1,117 @@
+<?php
+namespace Everyman\Neo4j\Gremlin;
+
+use Everyman\Neo4j\Client,
+	Everyman\Neo4j\EntityMapper,
+    Everyman\Neo4j\Node,
+    Everyman\Neo4j\Relationship;
+
+class RowTest extends \PHPUnit_Framework_TestCase
+{
+	protected $client = null;
+
+	public function setUp()
+	{
+		$this->client = new Client($this->getMock('Everyman\Neo4j\Transport', array(), array(), '', false));
+	}
+	
+	public function testCount()
+	{
+		$columns = array('name','age');
+		$data = array('Brenda', 14);
+
+		$row = new Row($this->client, $columns, $data);
+		$this->assertEquals(2, count($row));
+	}
+	
+	public function testIterate()
+	{
+		$columns = array('name','age');
+		$data = array('Brenda', 14);
+
+		$row = new Row($this->client, $columns, $data);
+		$i = 0;
+		foreach($row as $columnName => $fieldValue) {
+			$this->assertEquals($columns[$i], $columnName);
+			$this->assertEquals($data[$i], $fieldValue);
+			$i++;
+		}
+	}
+	
+	public function testArrayAccess()
+	{
+		$columns = array('name','age');
+		$data = array('Brenda', 14);
+
+		$row = new Row($this->client, $columns, $data);
+		$i = 0;
+		foreach($columns as $column) {
+			$this->assertEquals(true, isset($row[$column]));
+			$this->assertEquals(true, isset($row[$i]));
+			$this->assertEquals($row[$column], $data[$i]);
+			$this->assertEquals($row[$i], $data[$i]);
+			$i++;
+		}
+		
+		$this->assertEquals(false, isset($row['blah']));
+		$this->assertEquals(false, isset($row[3]));
+	}
+
+	public function testArrayAccess_Set_ThrowsException()
+	{
+		$columns = array('name','age');
+		$data = array('Brenda', 14);
+		$row = new Row($this->client, $columns, $data);
+
+		$this->setExpectedException('BadMethodCallException');
+		$row['test'] = 'value';
+	}
+
+	public function testArrayAccess_Unset_ThrowsException()
+	{
+		$columns = array('name','age');
+		$data = array('Brenda', 14);
+		$row = new Row($this->client, $columns, $data);
+
+		$this->setExpectedException('BadMethodCallException');
+		unset($row['name']);
+	}
+	
+	public function testNodeCasting()
+	{
+		$columns = array('user');
+		$data = array(
+			array(
+				'data' => array(
+					'name' => 'Bob'
+				),
+				'self' => 'http://localhost/db/data/node/0'
+			));
+
+		
+		$row = new Row($this->client, $columns, $data);
+		$i = 0;
+		
+		$this->assertTrue($row['user'] instanceof Node);
+	}
+	
+	public function testRelationshipCasting()
+	{
+		$columns = array('user');
+		$data = array(
+			array(
+				'data' => array(
+					'name' => 'Bob'
+				),
+				'type' => 'KNOWS',
+				'start' => 'http://localhost/db/data/node/0', 
+				'end' => 'http://localhost/db/data/node/1', 
+				'self' => 'http://localhost/db/data/relationship/0'
+			));
+
+		
+		$row = new Row($this->client, $columns, $data);
+		
+		$this->assertTrue($row['user'] instanceof Relationship);
+	}
+}
