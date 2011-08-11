42dda79556c244d45e6b83d8f0fb4afdbb309bc8
Josh Adell <josh.adell@gmail.com>
2011-08-11 18:11:26 -0400

    Auto-save unsaved relationship endpoints.

--- lib/Everyman/Neo4j/Batch.php
+++ lib/Everyman/Neo4j/Batch.php
@@ -21,20 +21,6 @@ class Batch
 	}
 
 	/**
-	 * Add an entity to the batch to save
-	 *
-	 * @param PropertyContainer $entity
-	 * @return integer
-	 */
-	public function add(PropertyContainer $entity)
-	{
-		$opId = count($this->operands);
-		$this->operands[] = $entity;
-		
-		return $opId;
-	}
-
-	/**
 	 * Get the batch's client
 	 *
 	 * @return Client
@@ -42,5 +28,53 @@ class Batch
 	public function getClient()
 	{
 		return $this->client;
+	}
+
+	/**
+	 * Get the entity referenced by the given position
+	 *
+	 * @param integer $index
+	 * @return PropertyContainer
+	 */
+	public function getOperand($index)
+	{
+		return isset($this->operands[$index]) ? $this->operands[$index] : null;
+	}
+
+	/**
+	 * Add an entity to the batch to save
+	 *
+	 * @param PropertyContainer $entity
+	 * @return integer
+	 */
+	public function save(PropertyContainer $entity)
+	{
+		if ($entity instanceof Relationship) {
+			$this->checkAndSaveRelationshipEndpoints($entity);
+		}
+	
+		$opId = count($this->operands);
+		$this->operands[] = $entity;
+		
+		return $opId;
+	}
+	
+	/**
+	 * Check that a relationship's start and end don't need saving
+	 * Save them if they do
+	 *
+	 * @param Relationship $rel
+	 */
+	protected function checkAndSaveRelationshipEndpoints(Relationship $rel)
+	{
+		$start = $rel->getStartNode();
+		if ($start && !$start->hasId()) {
+			$this->save($start);
+		}
+		
+		$end = $rel->getEndNode();
+		if ($end && !$end->hasId()) {
+			$this->save($end);
+		}
 	}
 }
--- tests/unit/lib/Everyman/Neo4j/BatchTest.php
+++ tests/unit/lib/Everyman/Neo4j/BatchTest.php
@@ -17,7 +17,7 @@ class BatchTest extends \PHPUnit_Framework_TestCase
 		$this->assertSame($this->client, $this->batch->getClient());
 	}
 
-	public function testAdd_AllEntitiesIdentified_ReturnsIntegerOperationIndex()
+	public function testSave_PropertyContainerEntities_ReturnsIntegerOperationIndex()
 	{
 		$nodeA = new Node($this->client);
 		$nodeA->setId(123);
@@ -26,17 +26,32 @@ class BatchTest extends \PHPUnit_Framework_TestCase
 		$nodeB->setId(456);
 
 		$nodeC = new Node($this->client);
-		$nodeC->setId(789);
 
 		$rel = new Relationship($this->client);
 		$rel->setId(987)
 			->setStartNode($nodeA)
 			->setEndNode($nodeB);
 			
-		$this->assertEquals(0, $this->batch->add($nodeA));
-		$this->assertEquals(1, $this->batch->add($nodeB));
-		$this->assertEquals(2, $this->batch->add($nodeC));
-		$this->assertEquals(3, $this->batch->add($rel));
+		$this->assertEquals(0, $this->batch->save($nodeA));
+		$this->assertEquals(1, $this->batch->save($nodeB));
+		$this->assertEquals(2, $this->batch->save($nodeC));
+		$this->assertEquals(3, $this->batch->save($rel));
+	}
+	
+	public function testSave_RelationshipReferencesUnidentifiedNode_NodeAddedToOperands_ReturnsIntegerOperationIndex()
+	{
+		$nodeA = new Node($this->client);
+		$nodeB = new Node($this->client);
+
+		$rel = new Relationship($this->client);
+		$rel->setId(987)
+			->setStartNode($nodeA)
+			->setEndNode($nodeB);
+			
+		$this->assertEquals(2, $this->batch->save($rel));
+		
+		$this->assertSame($nodeA, $this->batch->getOperand(0));
+		$this->assertSame($nodeB, $this->batch->getOperand(1));
 	}
 }
 
