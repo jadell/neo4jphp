437b6ea6bd87165f53141cd8421c511ae5e9a3da
Josh Adell <josh.adell@gmail.com>
2013-03-06 20:43:27 -0500

    Fix indentation

--- lib/Everyman/Neo4j/Transport/Curl.php
+++ lib/Everyman/Neo4j/Transport/Curl.php
@@ -1,7 +1,7 @@
 <?php
 namespace Everyman\Neo4j\Transport;
 use Everyman\Neo4j\Transport as BaseTransport,
-    Everyman\Neo4j\Version;
+	Everyman\Neo4j\Version;
 
 /**
  * Class for communicating with an HTTP JSON endpoint
@@ -64,16 +64,16 @@ class Curl extends BaseTransport
 				break;
 
 			case self::POST :
-            case self::PUT :
-                $dataString = $this->encodeData($data);
-                $options[CURLOPT_CUSTOMREQUEST] = $method;
-                $options[CURLOPT_POSTFIELDS] = $dataString;
-                $options[CURLOPT_HTTPHEADER][] = 'Content-Length: '.strlen($dataString);
+			case self::PUT :
+				$dataString = $this->encodeData($data);
+				$options[CURLOPT_CUSTOMREQUEST] = $method;
+				$options[CURLOPT_POSTFIELDS] = $dataString;
+				$options[CURLOPT_HTTPHEADER][] = 'Content-Length: '.strlen($dataString);
 
-                if (self::POST == $method) {
-                    $options[CURLOPT_POST] = true;
-                }
-            break;
+				if (self::POST == $method) {
+					$options[CURLOPT_POST] = true;
+				}
+			break;
 		}
 
 		$ch = $this->getHandle();
