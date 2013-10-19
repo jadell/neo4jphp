c82482a6fbf376066b09e4406e123261ba2badf0
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2013-10-19 19:34:15 +0200

    Implemented addLabels with REST calls

--- lib/Everyman/Neo4j/Command/AddLabels.php
+++ lib/Everyman/Neo4j/Command/AddLabels.php
@@ -1,7 +1,7 @@
 <?php
 namespace Everyman\Neo4j\Command;
 
-use Everyman\Neo4j\Command\SetLabels,
+use Everyman\Neo4j\Command,
 	Everyman\Neo4j\Client,
 	Everyman\Neo4j\Node,
 	Everyman\Neo4j\Label;
@@ -9,8 +9,11 @@ use Everyman\Neo4j\Command\SetLabels,
 /**
  * Add a set of labels to a node
  */
-class AddLabels extends SetLabels
+class AddLabels extends Command
 {
+	protected $node;
+	protected $labels;
+
 	/**
 	 * Set the labels to add
 	 *
@@ -18,9 +21,79 @@ class AddLabels extends SetLabels
 	 * @param Node   $node
 	 * @param array  $labels
 	 */
-	public function __construct(Client $client, Node $node, $labels)
+	public function __construct(Client $client, Node $node, Array $labels)
 	{
-		parent::__construct($client, $node, $labels, false);
+		if (!$labels) {
+			throw new \InvalidArgumentException("No labels given to set on node");
+		}
+
+		if (!$client->hasCapability(Client::CapabilityLabel)) {
+			throw new \RuntimeException('The connected Neo4j version does not have label capability');
+		}
+
+		parent::__construct($client);
+		$this->node = $node;
+		$this->labels = $labels;
+	}
+
+	/**
+	 * Return the data to pass
+	 *
+	 * @return mixed
+	 */
+	protected function getData()
+	{
+		$data = array_map(function ($label) {
+			if (!($label instanceof Label)) {
+				throw new \InvalidArgumentException("Cannot set a non-label");
+			}
+			return $label->getName();
+		}, $this->labels);
+
+		return $data;
+	}
+
+	/**
+	 * Return the transport method to call
+	 *
+	 * @return string
+	 */
+	protected function getMethod()
+	{
+		return 'post';
+	}
+
+	/**
+	 * Return the path to use
+	 *
+	 * @return string
+	 */
+	protected function getPath()
+	{
+		$nodeId = $this->node->getId();
+		if (!$nodeId) {
+			throw new \InvalidArgumentException("Cannot set labels on an unsaved node");
+		}
+				
+		return '/node/'.$nodeId.'/labels';
+	}
+
+	/**
+	 * Use the results
+	 *
+	 * @param integer $code
+	 * @param array   $headers
+	 * @param array   $data
+	 * @return boolean true on success
+	 * @throws Exception on failure
+	 */
+	protected function handleResult($code, $headers, $data)
+	{
+		if ((int)($code / 100) != 2) {
+			$this->throwException('Unable to add labels', $code, $headers, $data);
+		}
+
+		return true;
 	}
 }
 
