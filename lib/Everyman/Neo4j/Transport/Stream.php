c828526e68480c02dcfb887b9090b76e1a409c3e
Alex Bilbie <hello@alexbilbie.com>
2013-10-22 12:12:40 +0100

    Throw an exception instead if PHP can't connect

--- lib/Everyman/Neo4j/Transport/Stream.php
+++ lib/Everyman/Neo4j/Transport/Stream.php
@@ -1,7 +1,8 @@
 <?php
 namespace Everyman\Neo4j\Transport;
 use Everyman\Neo4j\Transport as BaseTransport,
-    Everyman\Neo4j\Version;
+	Everyman\Neo4j\Version,
+	Everyman\Neo4j\Exception;
 
 /**
  * Class for communicating with an HTTP JSON endpoint over PHP streams
@@ -47,23 +48,22 @@ class Stream extends BaseTransport
 
 		$context = stream_context_create($context_options);
 
-		// Capture the E_WARNING emitted if fopen fails
-		$phpError = null;
-		set_error_handler(function($errorNo, $errorMsg, $errorFile) use ($phpError) { $phpError = $errorMsg; });
+		// Throw an exception if fopen fails
+		set_error_handler(function($errorNo, $errorMsg, $errorFile) { throw new Exception("Can't open connection to ".$url); });
 
+		// Open the connection
 		$fh = fopen($url, 'r', false, $context);
 
 		// Restore the normal PHP error handler
 		restore_error_handler();
 
-		if ($phpError === null) {
-		    $response = '';
-		    // Capture the stream
-		    while (!feof($fh)) {
-		        $response .= fread($fh, 8192);
-		    }
-		    fclose($fh);
+		$response = '';
+
+		// Capture the stream
+		while (!feof($fh)) {
+			$response .= fread($fh, 8192);
 		}
+		fclose($fh);
 
 		// Catch error
 		preg_match('/^HTTP\/1\.[0-1] (\d{3})/', $http_response_header[0], $parts);
